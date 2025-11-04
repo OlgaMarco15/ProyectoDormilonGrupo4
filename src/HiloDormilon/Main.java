88c16b33186ccde894462b2abe64576db6fff5a8
GuillermoMartin <guillemsilgadod@gmail.com>
2025-11-04 13:21:30 +0100

    Tabulaciones para comentarios agregadas

--- src/HiloDormilon/Main.java
+++ src/HiloDormilon/Main.java
@@ -9,10 +9,13 @@ class HiloDormilon extends Thread {
     public void run() {
         try{
             Thread dormilon = new Thread();
+
             dormilon.start();
             System.out.println("Uyyyy que sueeñoo me voy a dormir...");
+
             dormilon.sleep(30000);
             System.out.println("Zzzzzzzzz");
+
         }catch (InterruptedException e){
             System.out.println("¡Uy va!, me han despertado");
         }
@@ -22,11 +25,15 @@ class HiloDormilon extends Thread {
 public class Main {
     public static void main(String[] args) {
         try {
+
             Thread dormilon = new HiloDormilon();
             dormilon.start();
+
             dormilon.sleep(2000);
             System.out.println("Estado del hilo: " + dormilon.getState());
+
             dormilon.interrupt();
+
             dormilon.join();
             System.out.println("Estado del hilo: " + dormilon.getState());
 
